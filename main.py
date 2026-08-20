4ef83cd01c17f84f5a5eeab06cd19209726b15ec
Sanchita <lilyblossm3@gmail.com>
2026-08-20 22:57:10 +0600

    Add /predict endpoint with sentiment analysis model

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
 
 app=FastAPI()
+sentiment_model=pipeline("sentiment-analysis")
+
+class TextInput(BaseModel):
+    text:str
 
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
+@app.post("/predict")
+def predict_sentiment(input: TextInput):
+
+    result = sentiment_model(input.text)
+    return{"result":result}
